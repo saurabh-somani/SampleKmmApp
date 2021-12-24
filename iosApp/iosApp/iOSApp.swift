fbc43c51e1cfcf9a1bba4c2b2747f1f14b024ea6
Saurabh Somani <50082769+saurabh-somani@users.noreply.github.com>
2021-12-24 12:21:33 +0530

    Injected Koin into iOS app.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,19 @@
 import SwiftUI
 import shared
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+            [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        CommonModuleKt.doInitKoin()
+        return true
+    }
+}
+
 @main
 struct iOSApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
